60d8378985e90d20ed2492c26daf4318306b90fa
CharlotteLouise <150289744+Macklou@users.noreply.github.com>
2023-11-30 19:59:06 +0000

    update js for bug error

--- javascript/index.js
+++ javascript/index.js
@@ -4,19 +4,13 @@ function changeBackground() {
   document.body.style.backgroundImage = selectedCity
     ? "url('css/" + selectedCity + "')"
     : "none";
+
+  updateLiveTime(selectedCity);
 }
+
 function updateLiveTime(city) {
   var now = moment().tz(city);
   var liveTime = now.format("h:mm:ss A");
   document.getElementById("liveTime" + city.replace(" ", "")).textContent =
     "Live Time: " + liveTime;
-}
-function changeBackground() {
-  var citySelector = document.getElementById("citySelector");
-  var selectedCity = citySelector.options[citySelector.selectedIndex].value;
-  document.body.style.backgroundImage = selectedCity
-    ? "url('css/" + selectedCity + "')"
-    : "none";
-
-  updateLiveTime(selectedCity);
 }
