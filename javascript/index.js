714f579cb2103ad2622d691914153d95bfe6305b
CharlotteLouise <150289744+Macklou@users.noreply.github.com>
2023-11-30 19:56:24 +0000

    update to live time

--- javascript/index.js
+++ javascript/index.js
@@ -5,3 +5,18 @@ function changeBackground() {
     ? "url('css/" + selectedCity + "')"
     : "none";
 }
+function updateLiveTime(city) {
+  var now = moment().tz(city);
+  var liveTime = now.format("h:mm:ss A");
+  document.getElementById("liveTime" + city.replace(" ", "")).textContent =
+    "Live Time: " + liveTime;
+}
+function changeBackground() {
+  var citySelector = document.getElementById("citySelector");
+  var selectedCity = citySelector.options[citySelector.selectedIndex].value;
+  document.body.style.backgroundImage = selectedCity
+    ? "url('css/" + selectedCity + "')"
+    : "none";
+
+  updateLiveTime(selectedCity);
+}
