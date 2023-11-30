9c6112f4e9378aeb9347d50d6c4c2b5b144d305e
CharlotteLouise <150289744+Macklou@users.noreply.github.com>
2023-11-30 20:05:49 +0000

    update to show live time

--- javascript/index.js
+++ javascript/index.js
@@ -8,9 +8,43 @@ function changeBackground() {
   updateLiveTime(selectedCity);
 }
 
-function updateLiveTime(city) {
-  var now = moment().tz(city);
-  var liveTime = now.format("h:mm:ss A");
-  document.getElementById("liveTime" + city.replace(" ", "")).textContent =
-    "Live Time: " + liveTime;
+function updateTime() {
+  let londonElement = document.querySelector("#los-angeles");
+  if (londonElement) {
+    let londonDateElement = londonElement.querySelector(".date");
+    let londonlesTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment().tz("Europe/London");
+
+    londonDateElement.innerHTML = londonTime.format("MMMM	Do YYYY");
+    londonTimeElement.innerHTML = londonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+      "A"
+    )}</small></div>
+  </div>
+  `;
+  }
+
+  updateTime();
+  setInterval(updateTime, 1000);
+
+  let citiesSelectElement = document.querySelector("#city");
+  citiesSelectElement.addEventListener("change", updateCity);
 }
