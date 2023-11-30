6b1f732d927f5626c34bd332022b6b6acea1676a
CharlotteLouise <150289744+Macklou@users.noreply.github.com>
2023-11-30 19:37:55 +0000

    update background img

--- jss/index.js
+++ jss/index.js
@@ -1,13 +1,7 @@
 function changeBackground() {
   var citySelector = document.getElementById("citySelector");
-  var selectedOption = citySelector.options[citySelector.selectedIndex];
-  var selectedCity = selectedOption.getAttribute("data-image");
-
+  var selectedCity = citySelector.options[citySelector.selectedIndex].value;
   document.body.style.backgroundImage = selectedCity
     ? "url('" + selectedCity + "')"
     : "none";
-
-  updateCityInfo(selectedOption.value);
 }
-
-function updateCityInfo(city) {}
